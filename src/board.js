ac0785ccc086d1f54fed8421f1641713548de278
Stephen Leander <stephen.leander1@gmail.com>
2022-05-25 15:43:45 +0200

    Add receiveAttack function

--- src/board.js
+++ src/board.js
@@ -2,7 +2,7 @@ import { createShip } from './ship';
 
 export function createBoard(width, height) {
   let board = {};
-  let ships = [];
+  board.ships = [];
   board.width = width;
   board.height = height;
 
@@ -18,10 +18,25 @@ export function createBoard(width, height) {
 
   board.addShip = function (length, x, y) {
     let ship = createShip(length);
-    ships.push(ship);
+    board.ships.push(ship);
     for (let i = 0; i < length; i++) {
       board.grid[y - 1][x - 1 + i] = ship.name;
     }
+  };
+
+  board.receiveAttack = function (x, y) {
+    let hitCoord = board.grid[y - 1][x - 1];
+    if (hitCoord === '') {
+      hitCoord = 'miss';
+    } else {
+      board.ships.forEach((ship) => {
+        if (hitCoord === ship.name) {
+          ship.hit();
+        }
+      });
+      hitCoord = 'hit';
+    }
+    return (board.grid[y - 1][x - 1] = hitCoord);
   };
 
   return board;
--- src/board.test.js
+++ src/board.test.js
@@ -18,3 +18,20 @@ test('Add ship to board at specified coordinates', () => {
   expect(testBoard.grid[3][6]).toBe('Cruiser');
   expect(testBoard.grid[3][7]).toBe('');
 });
+
+test('Receive attack at occupied coordinate and register hit', () => {
+  testBoard.addShip(3, 1, 1);
+  testBoard.receiveAttack(1, 1);
+  expect(testBoard.grid[0][0]).toBe('hit');
+});
+
+test('Increase hitCount of ship when hit is registered', () => {
+  testBoard.addShip(3, 1, 1);
+  testBoard.receiveAttack(2, 1);
+  expect(testBoard.ships[0].hitCount).toBe(1);
+});
+
+test('Receive attack at empty coordinate and register miss', () => {
+  testBoard.receiveAttack(1, 1);
+  expect(testBoard.grid[0][0]).toBe('miss');
+});
