c6d62d25b167c8676287875d7eb93245f49ff6ec
Stephen Leander <stephen.leander1@gmail.com>
2022-05-25 11:54:48 +0200

    Name ship according to length

--- src/ship.js
+++ src/ship.js
@@ -5,6 +5,18 @@ export function createShip(length) {
   ship.hitCount = 0;
   ship.isSunk = false;
 
+  if (ship.length === 1) {
+    ship.name = 'Submarine';
+  } else if (ship.length === 2) {
+    ship.name = 'Destroyer';
+  } else if (ship.length === 3) {
+    ship.name = 'Cruiser';
+  } else if (ship.length === 4) {
+    ship.name = 'Battleship';
+  } else if (ship.length === 5) {
+    ship.name = 'Aircraft Carrier';
+  }
+
   ship.hit = function () {
     ship.hitCount += 1;
     ship.checkSunk();
--- src/ship.test.js
+++ src/ship.test.js
@@ -9,6 +9,14 @@ test('Create new ship object with specified length', () => {
   expect(testShip.length).toBe(3);
 });
 
+test('Name ship according to length', () => {
+  expect(createShip(1).name).toBe('Submarine');
+  expect(createShip(2).name).toBe('Destroyer');
+  expect(createShip(3).name).toBe('Cruiser');
+  expect(createShip(4).name).toBe('Battleship');
+  expect(createShip(5).name).toBe('Aircraft Carrier');
+});
+
 test('Mark ship as sunk when hitCount = length', () => {
   testShip.hit();
   testShip.hit();
