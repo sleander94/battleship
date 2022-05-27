a38e94bc32054db2db5aa62f8267ffc8d3cdfc0c
Stephen Leander <stephen.leander1@gmail.com>
2022-05-27 16:53:58 +0200

    Create functions to generate page and manipulate DOM

--- src/dom-interaction.js
+++ src/dom-interaction.js
@@ -1,14 +1,31 @@
-export function createGameArea(element) {
+export function renderPage(element) {
+  const header = document.createElement('h1');
+  header.textContent = 'Battleship';
+  element.appendChild(header);
   const gameArea = document.createElement('div');
   gameArea.classList.toggle('gameArea');
   element.appendChild(gameArea);
+  const messageContainer = document.createElement('div');
+  messageContainer.classList.toggle('messageContainer');
+  element.appendChild(messageContainer);
 }
 
 export function renderBoard(player, element) {
   const boardDiv = document.createElement('div');
-  boardDiv.classList.toggle('board');
-  boardDiv.id = `${player.type}`;
+  boardDiv.classList.toggle('boardContainer');
 
+  const boardHeader = document.createElement('div');
+  boardHeader.classList.toggle('boardHeader');
+  if (player.type === 'computer') {
+    boardHeader.textContent = 'Enemy Enemy';
+  } else {
+    boardHeader.textContent = 'Your Fleet';
+  }
+  boardDiv.appendChild(boardHeader);
+
+  const board = document.createElement('div');
+  board.classList.toggle('board');
+  board.id = `${player.type}`;
   player.board.grid.forEach((row) => {
     const rowDiv = document.createElement('div');
     rowDiv.classList.toggle('row');
@@ -25,43 +42,63 @@ export function renderBoard(player, element) {
       rowDiv.appendChild(cellDiv);
       i += 1;
     });
-    boardDiv.appendChild(rowDiv);
+    board.appendChild(rowDiv);
   });
+  boardDiv.appendChild(board);
   element.appendChild(boardDiv);
 }
 
-export function listenForAttack(player, opponent, element) {
+export function startAttackLoop(player, opponent, element) {
+  const messageContainer = document.querySelector('.messageContainer');
   let opponentBoard = document.getElementById(`${opponent.type}`);
   let rows = opponentBoard.children;
   for (let i = 0; i < rows.length; i++) {
     for (let n = 0; n < rows[i].children.length; n++) {
       let cell = rows[i].children[n];
       cell.addEventListener('click', () => {
+        if (
+          player.makeAttack(
+            opponent,
+            cell.getAttribute('x'),
+            cell.getAttribute('y')
+          ) === true
+        ) {
+          opponent.makeAttack(player);
+        }
         player.makeAttack(
           opponent,
           cell.getAttribute('x'),
           cell.getAttribute('y')
         );
-        opponent.makeAttack(player);
         element.innerHTML = '';
         renderBoard(player, element);
         renderBoard(opponent, element);
-        if (checkGameOver(player, opponent)) {
-          console.log('gameover');
+        if (checkWinner(player, opponent) === 'computer') {
+          renderWinMessage(opponent, messageContainer);
+        } else if (checkWinner(player, opponent) === 'human') {
+          renderWinMessage(player, messageContainer);
         } else {
-          listenForAttack(player, opponent, element);
+          startAttackLoop(player, opponent, element);
         }
       });
     }
   }
 }
 
-function checkGameOver(player, computer) {
+function checkWinner(player, computer) {
   if (computer.board.fleetSunk() === true) {
-    alert('You win');
-    return true;
+    return 'human';
   } else if (player.board.fleetSunk() === true) {
-    alert('Computer wins');
-    return true;
+    return 'computer';
   }
+}
+
+function renderWinMessage(player, element) {
+  const message = document.createElement('div');
+  if (player.type === 'computer') {
+    message.textContent = 'Your fleet is destroyed. You lose.';
+  } else if (player.type === 'human') {
+    message.textContent = 'You have destroyed the enemy fleet. You win.';
+  }
+  element.appendChild(message);
 }
